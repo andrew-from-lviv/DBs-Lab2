47d4e91c53c5d4d2633a6334172e157284ef2ffc
Andriy Kusyy <a.kusyy@ucu.edu.ua>
2018-01-21 16:43:20 +0200

    Added revert for failed transactions

--- src/two_phase_commit.py
+++ src/two_phase_commit.py
@@ -1,5 +1,4 @@
 import psycopg2
-from psycopg2.extensions import connection, ISOLATION_LEVEL_READ_COMMITTED
 
 from src.config import *
 
@@ -17,37 +16,60 @@ class TwoPahaseCommit:
         except Exception as e:
             print(e)
 
+    def execute_commands(self, connections, commands):
+        if len(connections) != len(commands):
+            raise Exception("Some bad configuration")
+
+        for i in range(0, len(connections)):
+            connections[i].cursor().execute(commands[i])
+            connections[i].tpc_prepare()
+
+    def commit_transactions(self, transactions):
+        for transaction in transactions:
+            transaction.tpc_commit()
+
+
+    def close_connections(self, connections):
+        for connection in connections:
+            connection.close()
+
+
+    def rollback_transactions(self, connections):
+        for conn in connections:
+            conn.tpc_rollback()
+
+
     def do_work(self):
         try:
-            self.setup_transaction()
+            connections, commands = self.setup_transaction()
 
-            self.flight_connection.cursor().execute(CREATE_FLIGHT_COMMAND)
-            self.flight_connection.tpc_prepare()
+            self.execute_commands(connections, commands)
 
-            self.booking_connection.cursor().execute(CREATE_HOTEL_BOOKING_COMMAND)
-            self.booking_connection.tpc_prepare()
+            self.commit_transactions(connections)
 
-            self.bank_connection.cursor().execute(TAKE_MONEY_COMMAND)
-            self.bank_connection.tpc_prepare()
-
-            self.bank_connection.tpc_commit()
-            self.booking_connection.tpc_commit()
-            self.flight_connection.tpc_commit()
-
-            self.flight_connection.close()
-            self.booking_connection.close()
-            self.bank_connection.close()
+            self.close_connections(connections)
         except Exception as e:
             print(e)
-            self.flight_connection.tpc_rollback()
-            self.bank_connection.tpc_rollback()
-            self.booking_connection.tpc_rollback()
+            self.rollback_transactions(connections)
+            self.close_connections(connections)
 
 
     def setup_transaction(self):
-        self.bank_connection.tpc_begin(self.bank_connection.xid(XID, 'trID', 'conn1'))
+        connections = []
         self.booking_connection.tpc_begin(self.booking_connection.xid(XID, 'trID', 'conn2'))
+        connections.append(self.booking_connection)
         self.flight_connection.tpc_begin(self.flight_connection.xid(XID, 'trID', 'conn3'))
+        connections.append(self.flight_connection)
+        self.bank_connection.tpc_begin(self.bank_connection.xid(XID, 'trID', 'conn1'))
+        connections.append(self.bank_connection)
+
+        commands = [CREATE_HOTEL_BOOKING_COMMAND, CREATE_FLIGHT_COMMAND, TAKE_MONEY_COMMAND]
+
+        return connections, commands
+
+
+tpc = TwoPahaseCommit()
+tpc.do_work()
 
 
 
